8cafb122e9160436549308dc8afb343f0428a979
DanielTseng2004 <112913131+DanielTseng2004@users.noreply.github.com>
2024-10-28 16:04:13 +0800

    1028

---
+++ ts_prac/hw1.js
@@ -0,0 +1,5 @@
+var age = 25;
+var Name = "Alice";
+//age = 18;
+Name = "Jack";
+console.log(age, Name);
---
+++ ts_prac/hw1.ts
@@ -0,0 +1,6 @@
+const age: number = 25;
+let Name: string = "Alice";
+
+age = 18;
+Name = "Jack";
+console.log(age,Name);
